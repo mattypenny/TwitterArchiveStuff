7a8548947a2e2f96ffe756682b4cae099205cd60
matty penny <mattypenny@gmail.com>
2024-10-04 17:31:28 +0100

    added in effect a list of things I need to test

--- tests/TwitterArchive.tests.ps1
+++ tests/TwitterArchive.tests.ps1
@@ -2,11 +2,30 @@ Describe "Convert-TaTwitterArchiveToPsObject" {
 
     BeforeAll {
         
-        $tweets = cat ../tests/DummyTwitterArchive.json | convertfrom-json | Convert-TaTwitterArchiveToPsObject
+        $tweets = Get-Content tests/DummyTwitterArchive.json | convertfrom-json | Convert-TaTwitterArchiveToPsObject
 
     }
 
     It "returns the right number of tweets" {
         $tweets.length | Should -Be 1
     }
+
+        It -pending "converts shortened URLs to long URLs" {
+
+            $True | Should -Be $False
+
+    }
+
+    It -pending "includes an markdown image link in the output, if there is one" {
+
+            $True | Should -Be $False
+
+    }
+
+    It -pending "includes two image links in the text if there are two images" {
+
+            $True | Should -Be $False
+
+    }
+
 }
