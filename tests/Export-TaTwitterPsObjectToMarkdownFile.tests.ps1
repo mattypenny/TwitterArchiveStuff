ebe7aa9f31369ba0891c74b5469d23dbe7f66e02
matty penny <mattypenny@gmail.com>
2024-10-16 18:29:52 +0100

    Empty files...but also laying out what will happen, maybe

---
+++ tests/Export-TaTwitterPsObjectToMarkdownFile.tests.ps1
@@ -0,0 +1,19 @@
+function Export-TaTwitterPsObjectToMarkdownFile {
+<#
+.SYNOPSIS
+   xx
+#>
+   [CmdletBinding()]
+   param (
+   
+   )
+   
+   $DebugPreference = $PSCmdlet.GetVariableValue('DebugPreference')
+   
+   write-startfunction
+   
+   
+   write-endfunction
+   
+   
+}
